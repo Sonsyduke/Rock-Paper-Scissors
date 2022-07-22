5036e3081276aff71a1be60223a04c9718c1e016
Douglas Lombard <douglaslombard29@gmail.com>
2022-07-22 12:56:22 +0200

    Edited the rules pop-up

--- script.js
+++ script.js
@@ -15,61 +15,61 @@
 // Divide and conquer
 
 // (1 === Rock, 2 === Paper, 3 === Scissors)
-function computerSelection() {
-  let computerChoice = Math.floor(Math.random() * 3) + 1;
-  if (computerChoice === 1) {
-    return "rock";
-  } else if (computerChoice === 2) {
-    return "paper";
-  } else if (computerChoice === 3) {
-    return "scissors";
-  }
-}
+// function computerSelection() {
+//   let computerChoice = Math.floor(Math.random() * 3) + 1;
+//   if (computerChoice === 1) {
+//     return "rock";
+//   } else if (computerChoice === 2) {
+//     return "paper";
+//   } else if (computerChoice === 3) {
+//     return "scissors";
+//   }
+// }
 
 // Create a playerSelection function
-function playerSelection() {
-  let playerChoice = prompt("Pick between Rock, Paper, Scissors");
-  playerChoice = playerChoice.toLowerCase();
+// function playerSelection() {
+//   let playerChoice = prompt("Pick between Rock, Paper, Scissors");
+//   playerChoice = playerChoice.toLowerCase();
 
-  while (
-    !(
-      playerChoice == "rock" ||
-      playerChoice == "paper" ||
-      playerChoice == "scissors"
-    )
-  ) {
-    playerChoice = prompt(
-      "Invalid selection. Pick between rock, paper, scissors"
-    );
-    playerChoice = playerChoice.toLowerCase();
-  }
-  return playerChoice;
-}
+//   while (
+//     !(
+//       playerChoice == "rock" ||
+//       playerChoice == "paper" ||
+//       playerChoice == "scissors"
+//     )
+//   ) {
+//     playerChoice = prompt(
+//       "Invalid selection. Pick between rock, paper, scissors"
+//     );
+//     playerChoice = playerChoice.toLowerCase();
+//   }
+//   return playerChoice;
+// }
 
-function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
-  if (playerSelection === computerSelection) {
-    return `It's a draw`;
-  } else if (playerSelection === "rock" && computerSelection === "scissors") {
-    return `You win! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "rock" && computerSelection === "paper") {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection === "paper" && computerSelection === "rock") {
-    return `You win! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "paper" && computerSelection === "scissors") {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
-  } else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return `You win! ${playerSelection} beats ${computerSelection}`;
-  } else if (playerSelection === "scissors" && computerSelection === "rock") {
-    return `You lose! ${computerSelection} beats ${playerSelection}`;
-  }
-}
+// function playRound(playerSelection, computerSelection) {
+//   playerSelection = playerSelection.toLowerCase();
+//   if (playerSelection === computerSelection) {
+//     return `It's a draw`;
+//   } else if (playerSelection === "rock" && computerSelection === "scissors") {
+//     return `You win! ${playerSelection} beats ${computerSelection}`;
+//   } else if (playerSelection === "rock" && computerSelection === "paper") {
+//     return `You lose! ${computerSelection} beats ${playerSelection}`;
+//   } else if (playerSelection === "paper" && computerSelection === "rock") {
+//     return `You win! ${playerSelection} beats ${computerSelection}`;
+//   } else if (playerSelection === "paper" && computerSelection === "scissors") {
+//     return `You lose! ${computerSelection} beats ${playerSelection}`;
+//   } else if (playerSelection === "scissors" && computerSelection === "paper") {
+//     return `You win! ${playerSelection} beats ${computerSelection}`;
+//   } else if (playerSelection === "scissors" && computerSelection === "rock") {
+//     return `You lose! ${computerSelection} beats ${playerSelection}`;
+//   }
+// }
 
-function game() {
-  for (let i = 0; i < 5; i++) {
-    console.log(playRound(playerSelection(), computerSelection()));
-  }
-}
-game();
+// function game() {
+//   for (let i = 0; i < 5; i++) {
+//     console.log(playRound(playerSelection(), computerSelection()));
+//   }
+// }
+// game();
 
 // console.log(playRound(playerSelection(), computerSelection()));
